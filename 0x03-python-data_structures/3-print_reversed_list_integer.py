cc2b6b79c0995af75f793f0e310807c3badcd4f1
James-Kwao <jameskwaw1@gmail.com>
2022-06-06 15:17:43 -0700

    Initial commit

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-"""
-reverse int
-"""
-
-
 def print_reversed_list_integer(my_list=[]):
-
-        if not my_list:
-                return None
-        for x in my_list[::-1]:
-                print("{:d}".format(x))
+    if my_list:
+        my_list.reverse()
+        for i in range(len(my_list)):
+            print("{:d}".format(my_list[i]))
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-
-
 def print_matrix_integer(matrix=[[]]):
-    for row in range(len(matrix)):
-        for column in range(len(matrix[row])):
-            print("{:d}".format(matrix[row][column]), end=" ")
-            if column != len(matrix[row]) - 1:
-                print("{}".format(""), end=" ")
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
         print()
