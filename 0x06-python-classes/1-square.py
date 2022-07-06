ddd1d4237141662a6d3278da8b057fdfb958a90e
James-Kwao <jameskwaw1@gmail.com>
2022-07-05 17:06:09 -0700

    Initial commit

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-class Square:
-    """Represents a square.
-    Private instance attribute: size.
-    Instantiation with size (no type/value verification).
-    """
+"""Class Square with size"""
 
-    def __init__(self, size):
-        """Initializes the data."""
-        self.__size = size
+
+class Square:
+    """Class to create a square with it size"""
+
+    def __init__(self, square_size):
+        """Constructor of the Square"""
+        self.__size = square_size
